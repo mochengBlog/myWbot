300e0451b93594e8468b038a7eb6dda747954c82
liushuo <shuo.liu@ycwh.cc>
2024-07-05 14:43:10 +0800

    fix 测试图片

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ def main(chat_type: int):
     # robot.enableRecvMsg()     # 可能会丢消息？
     robot.enableReceivingMsg()  # 加队列
 
-    send_image()
+    send_image(robot)
     # 每天 7 点发送天气预报
     robot.onEveryTime("17:00", weather_report, robot=robot)
 
